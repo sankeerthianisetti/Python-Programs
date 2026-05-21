048e5b9dcb25c95855d3e8d522348a4d7bda4ad7
sankeerthianisetti <152169532+sankeerthianisetti@users.noreply.github.com>
2026-05-21 17:17:43 +0530

    Add basic student and circle details program

---
+++ basics.py
@@ -0,0 +1,18 @@
+dreamcompany="xyz company"
+PI=3.14159
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+radius=float(input("Enter circle radius: "))
+
+incage=age+1
+area=PI*radius*radius
+
+print("Student Details")
+print("Dream Company: ",dreamcompany)
+print("Student Name: ",name)
+print("Current Age: ",age)
+print("Next Year Age: ",incage)
+
+print("Circle Details")
+print("Radius: ",radius)
+print("Area: ",area)
